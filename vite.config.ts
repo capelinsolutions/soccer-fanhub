c4616af9e59be0ea97c12346c2bd74925c596d12
Zain ul Abideen <zain@theoriginationapi.com>
2026-06-23 10:40:12 +0500

    Added nitro

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
+    nitro: true,
     server: {
       entry: "server",
       preset: "vercel", // 🔥 ADD THIS
